c2a8f909216f798b6b3de9c77bad8052945373ee
Marcus <marcusdeleon1983@gmail.com>
2025-11-05 03:02:22 -0600

    Add module source

---
+++ src/Controllers/ElementCarouselController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Antlion\ElementCarousel\Controllers;
+
+use SilverStripe\View\Requirements;
+use DNADesign\Elemental\Controllers\ElementController;
+
+class ElementCarouselController extends ElementController
+{
+  public function init()
+  {
+    parent::init();
+
+    // Requirements::css('');
+    // Requirements::javascript('');
+  }
+}
